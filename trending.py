c798f128605ab7701edf9bfabd1aebe9ec92f850
Forest Gregg <fgregg@uchicago.edu>
2016-02-09 11:37:35 -0600

    better smoothing

--- trending.py
+++ trending.py
@@ -5,13 +5,13 @@ import datetime
 
 SMOOTHER = 20
 WINDOW = 8
-GROWTH_THRESHOLD = 0.02
+GROWTH_THRESHOLD = 0.03
 
 def trend(counts) :
     X, Y = zip(*counts)
 
     X = numpy.array([x.toordinal() for x in X])
-    X -= datetime.date.today().toordinal()
+    X -= datetime.date.today().toordinal() 
     A = numpy.array([numpy.ones(len(X)), X])
 
     Y = numpy.log(numpy.array(Y))
@@ -25,6 +25,12 @@ profile = ga.authenticate(identity='sunspot',
                           webproperty='Illinois Sunshine', 
                           profile='Illinois Sunshine')
 
+#profile = ga.authenticate(identity='sunspot', 
+#                          account='Councilmatic',
+#                          webproperty='Chicago Councilmatic', 
+#                          profile='Chicago Councilmatic')
+
+
 totals = profile.core.query.metrics('pageviews').\
                             daily(days=-WINDOW)
 
@@ -36,18 +42,20 @@ pages = profile.core.query.metrics('pageviews').\
 
 page_counts = collections.defaultdict(dict) 
 normalized_page_counts = collections.defaultdict(dict) 
+smooth_denom = SMOOTHER * len(pages.rows)
+
 
 for date, page, count in pages.rows :
     page_counts[page][date] = count
-    normalized_page_counts[page][date] = (count + SMOOTHER)/(totals[date] + SMOOTHER)
+    normalized_page_counts[page][date] = (count + SMOOTHER)/(totals[date] + smooth_denom)
 
 for counts in normalized_page_counts.values() :
     for date in totals.keys() - counts.keys() :
-        counts[date] = SMOOTHER/(totals[date] + SMOOTHER)
+        counts[date] = SMOOTHER/(totals[date] + smooth_denom)
 
 for page, counts in normalized_page_counts.items() :
     b0, b1 = trend(counts.items())
-    if b1 > GROWTH_THRESHOLD and page.startswith('/committees/')  :
+    if b1 > GROWTH_THRESHOLD : #and page.startswith('/committees/')  :
         print(page, b0, b1)
         for count in sorted(page_counts[page].items()) :
             print(count)
